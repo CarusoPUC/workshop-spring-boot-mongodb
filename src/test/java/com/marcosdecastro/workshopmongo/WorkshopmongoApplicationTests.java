be2ed6e457d02d934c54f0cd844c2065755421a9
Marcos <marcos.c.junior2@gmail.com>
2024-03-10 13:24:22 -0300

    Projeto criado

---
+++ src/test/java/com/marcosdecastro/workshopmongo/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcosdecastro.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
